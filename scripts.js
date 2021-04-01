b55b23979131fcd738a9cf936084b6cffcaf0ec2
Iuri Magnago <iurimp91@gmail.com>
2021-04-01 19:42:40 -0300

    Adicionado o aspecto de desmarcar uma opção quando outra é escolhida para os menus de bebida e sobremesa

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-function escolha(item, check, opcao) {
+let prato;
+let bebida;
+let sobremesa;
+
+function escolha1(item, check, opcao) {
+    prato = opcao;
     const selecionadoBorda = document.querySelector(".menu-prato .borda-verde");
     const selecionadoCheck = document.querySelector(".menu-prato .borda-verde ion-icon");
 
@@ -16,3 +21,44 @@ function escolha(item, check, opcao) {
     const escolhaCheck = document.querySelector(check);
     escolhaCheck.classList.remove("escondido");
 }
+
+function escolha2(item, check, opcao) {
+    bebida = opcao;
+    const selecionadoBorda = document.querySelector(".menu-bebida .borda-verde");
+    const selecionadoCheck = document.querySelector(".menu-bebida .borda-verde ion-icon");
+
+    if (selecionadoBorda !== null) {
+        selecionadoBorda.classList.remove("borda-verde");
+    }
+
+    if (selecionadoCheck !== null) {
+        selecionadoCheck.classList.add("escondido");
+    }
+
+    const escolhaBorda = document.querySelector(item);
+    escolhaBorda.classList.add("borda-verde");
+    
+    const escolhaCheck = document.querySelector(check);
+    escolhaCheck.classList.remove("escondido");
+}
+
+function escolha3(item, check, opcao) {
+    bebida = opcao;
+    const selecionadoBorda = document.querySelector(".menu-sobremesa .borda-verde");
+    const selecionadoCheck = document.querySelector(".menu-sobremesa .borda-verde ion-icon");
+
+    if (selecionadoBorda !== null) {
+        selecionadoBorda.classList.remove("borda-verde");
+    }
+
+    if (selecionadoCheck !== null) {
+        selecionadoCheck.classList.add("escondido");
+    }
+
+    const escolhaBorda = document.querySelector(item);
+    escolhaBorda.classList.add("borda-verde");
+    
+    const escolhaCheck = document.querySelector(check);
+    escolhaCheck.classList.remove("escondido");
+}
+
