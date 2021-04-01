f5c1ad585024b42f8854800b982de165520f9529
Iuri Magnago <iurimp91@gmail.com>
2021-04-01 19:24:34 -0300

    Adicionado o aspecto de desmarcar uma opção quando outra é escolhida, somente para o menu dos pratos

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-function escolha () {
-    const escolhaBorda = document.querySelector("li");
-    escolhaBorda.classList.toggle("check-borda");
+function escolha(item, check, opcao) {
+    const selecionadoBorda = document.querySelector(".menu-prato .borda-verde");
+    const selecionadoCheck = document.querySelector(".menu-prato .borda-verde ion-icon");
+
+    if (selecionadoBorda !== null) {
+        selecionadoBorda.classList.remove("borda-verde");
+    }
+
+    if (selecionadoCheck !== null) {
+        selecionadoCheck.classList.add("escondido");
+    }
+
+    const escolhaBorda = document.querySelector(item);
+    escolhaBorda.classList.add("borda-verde");
     
-    const escolhaCheck = document.querySelector("ion-icon");
-    escolhaCheck.classList.toggle("escondido");
+    const escolhaCheck = document.querySelector(check);
+    escolhaCheck.classList.remove("escondido");
 }
